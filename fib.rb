92f3100603167479630f1307b630ad728c9f37e3
Stephen Hughes <Hughes0687@gmail.com>
2014-07-10 16:08:43 -0500

    recursive with cache

--- fib.rb
+++ fib.rb
@@ -1,6 +1,16 @@
 def fib(n)
   return 1 if n <= 2
-  fib(n - 1) + fib(n - 2)
+  fib(n - 1) + fib(n - 2)  
+end
+
+$cache = []
+
+def fib_cache(n)
+  $cache[n] ||= if n < 2
+    n
+  else
+    fib_cache(n-1) + fib_cache(n-2)
+  end
 end
 
 def it_fib(n)
